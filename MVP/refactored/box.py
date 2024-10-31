23ec5535edc9e11a42cc6b08abdd011f56d4a6f7
mavrag <mavrag@taltech.ee>
2024-10-31 11:09:38 +0200

    #6 Deleting items

--- MVP/refactored/box.py
+++ MVP/refactored/box.py
@@ -56,7 +56,6 @@ class Box:
         self.canvas.tag_bind(self.resize_handle, '<ButtonPress-1>', self.on_resize_press)
         self.canvas.tag_bind(self.resize_handle, '<B1-Motion>', self.on_resize_drag)
         self.canvas.tag_bind(self.rect, '<Double-Button-1>', self.set_inputs_outputs)
-        self.canvas.tag_bind(self.rect, '<Delete>', lambda event: self.press_delete())
 
     def show_context_menu(self, event):
         self.close_menu()
@@ -523,8 +522,3 @@ class Box:
         if not self.has_right_connections():
             return 0
         return max([c.index if c.side == "right" else 0 for c in self.connections]) + 1
-
-    def press_delete(self):
-        print("do")
-        self.canvas.delete_selected_items()
-
