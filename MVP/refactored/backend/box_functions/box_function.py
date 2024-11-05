04105f24de0bc9d75d1c34668051e8eb346f85a2
Andrei <anmjas@ttu.ee>
2024-11-06 00:36:54 +0200

    little change

--- MVP/refactored/backend/box_functions/box_function.py
+++ MVP/refactored/backend/box_functions/box_function.py
@@ -1,8 +1,13 @@
+from inspect import signature
+
+
 def add(numbers: list) -> int:
     return sum(numbers)
 
+
 def subtract(numbers: list) -> int:
     return numbers[0] - sum(numbers[1:])
+
 
 functions = {
     "Copy": lambda x: [x, x],
@@ -10,34 +15,34 @@ functions = {
     "Subtract": subtract
 }
 
+
 class BoxFunction:
     def __init__(self, name, code):
         self.name = name
         self.code = code or functions.get(name)
+        if isinstance(self.code, str):
+            function = {}
+            exec(self.code, {}, function)
+            self.code = function["invoke"]
 
     def __call__(self, *args):
-        if callable(self.code):
-            return self.code(*args)
-        else:
-            function = {}
-            try:
-                exec(self.code, {}, function)
-                return function["invoke"](args)
-            except Exception as e:
-                print(f"Error executing code in '{self.name}':", e)
-                return None
+        return self.code(*args)
+
+    def count_inputs(self):
+        sig = signature(self.code)
+        params = sig.parameters
+        count = len(params)
+        if params["self"]:
+            count -= 1
+        return count
 
     def __str__(self):
         return self.name
 
 
 def test_box():
-    custom_code = """def call(*args):
+    custom_code = """def invoke(n, l, g, *args):
         return sum(args)
     """
-    function = {}
-    exec(custom_code, {}, function)
-
-    print(function["call"](3, 4, 5, 6))
-
-test_box()
+    function = BoxFunction("test", custom_code)
+    print(function.count_inputs())
