62089b7aece593601c297f704e7ba1502b7a8c4e
Andrei <anmjas@ttu.ee>
2024-11-06 17:26:25 +0200

    little change

---
+++ MVP/refactored/backend/box_functions/predefined/Add.py
@@ -0,0 +1,10 @@
+def add(numbers: list) -> int:
+    if len(numbers) < 2:
+        raise ValueError("Numbers amount should be atleast 2")
+    return sum(numbers)
+
+
+meta = {
+    "name": "Add",
+
+}
