8ca214dfeaacec53eb654b6616a209f05d036852
Andrei <anmjas@ttu.ee>
2024-11-14 00:59:32 +0200

    few refactored changes

--- MVP/refactored/backend/box_functions/predefined/add.py
+++ MVP/refactored/backend/box_functions/predefined/add.py
@@ -2,7 +2,7 @@ import math
 
 def invoke(*numbers: list) -> int:
     if len(numbers) < 2:
-        raise ValueError("Numbers amount should be atleast 2")
+        raise ValueError("Numbers amount should be at least 2")
     return sum(numbers)
 
 
--- MVP/refactored/backend/box_functions/predefined/subtract.py
+++ MVP/refactored/backend/box_functions/predefined/subtract.py
@@ -3,7 +3,7 @@ import math
 
 def invoke(*numbers: list) -> int:
     if len(numbers) < 2:
-        raise ValueError("Numbers amount should be atleast 2")
+        raise ValueError("Numbers amount should be at least 2")
     return numbers[0] - sum(numbers[1:])
 
 
