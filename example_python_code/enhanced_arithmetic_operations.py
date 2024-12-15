7df9a6f786349486ca9162a58fd19e54cdb109ee
Timofei Beresnjev <bertimblch@gmail.com>
2024-12-15 19:41:44 +0200

    Allow imported python file contain comments and prints.

--- example_python_code/enhanced_arithmetic_operations.py
+++ example_python_code/enhanced_arithmetic_operations.py
@@ -5,6 +5,7 @@ def multiply(a, b):
     return a * b, a
 
 def subtract(a, b):
+    print("Subtracting", a, b)
     return a - b
 
 def divide(a, b):
@@ -23,10 +24,13 @@ def floor_divide(a, b):
 
 if __name__ == "__main__":
     sum_result = add(10, 5)
+    # this is a comment
 
     product_result, _ = multiply(sum_result, 3)
+    print(product_result)
 
     diff_result = subtract(product_result, sum_result)
+    # this is another comment
 
     division_result = divide(product_result, 1)
 
