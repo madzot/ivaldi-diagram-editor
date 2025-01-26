074eb7a6a69b984bb0d41f7b8f74aa45d3815bc8
Mark Dzotsenidze <markdzo@hotmail.com>
2025-01-26 12:50:07 +0200

    #83 removing hupper for auto restarting on file change

--- MVP/refactored/launcher.py
+++ MVP/refactored/launcher.py
@@ -2,8 +2,6 @@ import tkinter as tk
 from tkinter import font  # Import the font module
 from tkinter import messagebox
 
-import hupper
-
 from MVP.refactored.backend.diagram_callback import Receiver
 from MVP.refactored.frontend.windows.main_diagram import MainDiagram
 
@@ -64,10 +62,5 @@ class Launcher:
         self.root.destroy()
 
 
-def start_program():
-    Launcher()
-
-
 if __name__ == '__main__':
-    hupper.start_reloader('launcher.start_program')
-    start_program()
+    Launcher()
