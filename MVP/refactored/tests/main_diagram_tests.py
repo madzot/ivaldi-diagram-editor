9b5698727844c8a91b20cbd4c928ec9a95d4df19
Mark Dzotsenidze <markdzo@hotmail.com>
2025-01-29 16:13:48 +0200

    #85 tree toggle, clipboard, del_from_canvas, add_canvas, switch_canvas tests

--- MVP/refactored/tests/main_diagram_tests.py
+++ MVP/refactored/tests/main_diagram_tests.py
@@ -1,6 +1,8 @@
+import tkinter
 import unittest
 
 from MVP.refactored.backend.diagram_callback import Receiver
+from MVP.refactored.frontend.components.custom_canvas import CustomCanvas
 from MVP.refactored.frontend.windows.main_diagram import MainDiagram
 
 
@@ -25,9 +27,8 @@ class Test(TestMainDiagram):
         self.assertEqual(expected, actual)
 
     def test__init_tree_hidden(self):
-        expected = False
         actual = self.app.is_tree_visible
-        self.assertEqual(expected, actual)
+        self.assertFalse(actual)
 
     def test__init_creates_1_custom_canvas(self):
         expected_size = 1
@@ -100,9 +101,50 @@ class Test(TestMainDiagram):
         actual = len(self.app.custom_canvas.outputs)
         self.assertEqual(expected, actual, "Outputs are not removed correctly")
 
-    # def test_load_functions():
-    #     assert False
-    #
+    def test__toggle_tree_view__changes_boolean(self):
+        actual = self.app.is_tree_visible
+        self.assertFalse(actual)
+        self.app.toggle_treeview()
+        actual = self.app.is_tree_visible
+        self.assertTrue(actual)
+
+    def test__copy_to_clipboard__copies_text_from_text_box(self):
+        text = tkinter.Text()
+        expected = "Lorem ipsum"
+        text.insert(tkinter.END, expected)
+        self.app.copy_to_clipboard(text)
+        actual = self.app.clipboard_get().strip()
+        self.assertEqual(expected, actual)
+
+    def test__del_from_canvasses__removes_dict_entry(self):
+        self.app.del_from_canvasses(self.app.canvasses[str(self.app.custom_canvas.id)])
+        expected = 0
+        actual = len(self.app.canvasses)
+        self.assertEqual(expected, actual)
+
+    def test__add_canvas__adds_new_canvas_to_canvasses_and_tree(self):
+        new_canvas = CustomCanvas(self.app, None, Receiver(), self.app, self.app.custom_canvas, False, id_=1)
+        self.app.add_canvas(new_canvas)
+
+        expected_size = 2
+        actual_size = len(self.app.canvasses)
+        self.assertEqual(expected_size, actual_size)
+
+        new_canvas_id = 1
+        self.assertTrue(self.app.canvasses[str(new_canvas_id)] == new_canvas)
+
+    def test__switch_canvas__changes_custom_canvas(self):
+        new_canvas = CustomCanvas(self.app, None, Receiver(), self.app, self.app.custom_canvas, False, id_=1)
+        self.app.add_canvas(new_canvas)
+        self.app.switch_canvas(new_canvas)
+
+        self.assertEqual(new_canvas, self.app.custom_canvas)
+
+
+
+
+
+
     #
     # def test_generate_code():
     #     assert False
