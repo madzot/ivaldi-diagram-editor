ddd01e515c7a12c33336cf8411152f0f6d2e4f54
mavrag <mavrag@taltech.ee>
2025-03-01 13:33:03 +0200

    #120 Make it easier to add key-binds and description

--- MVP/refactored/frontend/windows/help_window.py
+++ MVP/refactored/frontend/windows/help_window.py
@@ -18,81 +18,73 @@ class HelpWindow(Toplevel):
         self.keybind_frame = Frame(self)
         self.keybind_frame.pack(fill=tk.BOTH, expand=True)
 
-        font = ("Arial", 11)
+        self.font = ("Arial", 11)
 
-        key_binds = []
-        descriptions = []
+        self.key_binds_descriptions = [
+            ("CTRL + N", "Create a sub-diagram from selected items"),
+            ("CTRL + C", "Copy the selected items"),
+            ("CTRL + V", "Paste the copied items"),
+            ("CTRL + X", "Cut the selected items"),
+            ("CTRL + A", "Select all items"),
+            ("CTRL + F", "Open search window")
+        ]
+        self.items_per_page = 5
+        self.current_page = 0
 
-        self.sub_diagram_keybind = Label(self.keybind_frame, text="CTRL + N", font=font)
+        self.display_key_binds()
 
-        self.sub_diagram_text = Label(self.keybind_frame, text="Create a sub-diagram from selected items",
-                                      wraplength=200, justify=tk.CENTER, font=font)
-
-        self.copy_keybind = Label(self.keybind_frame, text="CTRL + C", font=font)
-
-        self.copy_text = Label(self.keybind_frame, text="Copy the selected items", wraplength=200, justify=tk.CENTER, font=font)
-
-        self.paste_keybind = Label(self.keybind_frame, text="CTRL + V", font=font)
-
-        self.paste_text = Label(self.keybind_frame, text="Paste the copied items", wraplength=200, justify=tk.CENTER, font=font)
-
-        self.search_keybind = Label(self.keybind_frame, text="CTRL + F", font=font)
-
-        self.search_text = Label(self.keybind_frame, text="Open search window", wraplength=200, justify=tk.CENTER, font=font)
-
-        key_binds.append(self.sub_diagram_keybind)
-        key_binds.append(self.copy_keybind)
-        key_binds.append(self.paste_keybind)
-        key_binds.append(self.search_keybind)
-
-        descriptions.append(self.sub_diagram_text)
-        descriptions.append(self.copy_text)
-        descriptions.append(self.paste_text)
-        descriptions.append(self.search_text)
-
-        ttk.Separator(self.keybind_frame, orient=tk.VERTICAL).grid(column=0, row=0, rowspan=10, sticky="nse")
-        ttk.Separator(self.keybind_frame, orient=tk.HORIZONTAL).grid(column=0, row=9, columnspan=2, sticky="ews")
-
-        self.keybind_frame.columnconfigure(0, weight=1)
-        self.keybind_frame.columnconfigure(1, weight=1)
-        for i in range(10):
-            self.keybind_frame.rowconfigure(i, weight=1)
-
-        for i in range(len(key_binds)):
-            keybind = key_binds[i]
-            keybind.grid(column=0, row=i)
-
-        for i in range(len(descriptions)):
-            description = descriptions[i]
-            description.grid(column=1, row=i)
+        ttk.Separator(self.keybind_frame, orient=tk.VERTICAL).grid(column=0, row=0, rowspan=6, sticky="nse")
+        ttk.Separator(self.keybind_frame, orient=tk.HORIZONTAL).grid(column=0, row=5, columnspan=2, sticky="ews")
 
         self.pagination_frame = Frame(self, pady=10)
         self.pagination_frame.pack(side=tk.BOTTOM, fill=tk.BOTH)
 
         # These pagination buttons are for the future when we need to have more than 1 page of help
 
-        self.backward_logo = (Image.open(ASSETS_DIR + "/chevron-left-circle-outline.png"))
+        self.backward_logo = Image.open(ASSETS_DIR + "/chevron-left-circle-outline.png")
         self.backward_logo = self.backward_logo.resize((35, 35))
         self.backward_logo = ImageTk.PhotoImage(self.backward_logo)
 
-        self.backward = Label(self.pagination_frame, image=self.backward_logo)
+        self.backward = tk.Button(self.pagination_frame, image=self.backward_logo, command=self.previous_page)
 
         self.forward_logo = (Image.open(ASSETS_DIR + "/chevron-right-circle-outline.png"))
         self.forward_logo = self.forward_logo.resize((35, 35))
         self.forward_logo = ImageTk.PhotoImage(self.forward_logo)
 
-        self.forward = Label(self.pagination_frame, image=self.forward_logo)
+        self.forward = tk.Button(self.pagination_frame, image=self.forward_logo, command=self.next_page)
 
         self.pagination_frame.columnconfigure(0, weight=1)
         self.pagination_frame.columnconfigure(1, weight=1)
 
-        self.pagination_frame.rowconfigure(9, weight=1)
-
         self.backward.grid(column=0, row=0, sticky="e", padx=(0, 15))
         self.forward.grid(column=1, row=0, sticky="w", padx=(15, 0))
 
+    def display_key_binds(self):
+        for widget in self.keybind_frame.winfo_children():
+            if isinstance(widget, Label):
+                widget.destroy()
 
+        start_idx = self.current_page * self.items_per_page
+        end_idx = start_idx + self.items_per_page
+        visible_key_binds = self.key_binds_descriptions[start_idx:end_idx]
 
+        for i, (keybind_text, description_text) in enumerate(visible_key_binds):
+            keybind_label = Label(self.keybind_frame, text=keybind_text, font=self.font)
+            description_label = Label(self.keybind_frame, text=description_text, wraplength=200, justify=tk.CENTER,
+                                      font=self.font)
 
+            keybind_label.grid(column=0, row=i)
+            description_label.grid(column=1, row=i)
+            self.keybind_frame.rowconfigure(i, minsize=80)
+            self.keybind_frame.columnconfigure(0, minsize=100, weight=1)
+            self.keybind_frame.columnconfigure(1, minsize=260, weight=1)
 
+    def next_page(self):
+        if (self.current_page + 1) * self.items_per_page < len(self.key_binds_descriptions):
+            self.current_page += 1
+            self.display_key_binds()
 
+    def previous_page(self):
+        if self.current_page > 0:
+            self.current_page -= 1
+            self.display_key_binds()
