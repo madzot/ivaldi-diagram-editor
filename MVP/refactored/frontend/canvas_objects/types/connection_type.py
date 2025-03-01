be28cff6f41907e0e5ad4fa284ee7c1929b929f5
Mark Dzotsenidze <markdzo@hotmail.com>
2025-03-01 13:02:44 +0200

    #118 new colors for connections

--- MVP/refactored/frontend/canvas_objects/types/connection_type.py
+++ MVP/refactored/frontend/canvas_objects/types/connection_type.py
@@ -6,8 +6,16 @@ class ConnectionType(Enum):
     FIRST = 1
     SECOND = 2
     THIRD = 3
+    FOURTH = 4
+    FIFTH = 5
+    SIXTH = 6
+    SEVENTH = 7
+    EIGHTH = 8
+    NINTH = 9
 
-    COLORS = ['black', 'magenta', 'cyan', 'firebrick1']
+    COLORS = ['black', 'magenta', 'cyan',
+              'firebrick1', 'gold2', 'RoyalBlue1',
+              'lime green', 'spring green', 'light slate blue']
 
     def next(self):
-        return ConnectionType((self.value + 1) % 4)
+        return ConnectionType((self.value + 1) % 9)
