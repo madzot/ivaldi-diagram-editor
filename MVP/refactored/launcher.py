e3372fa5db4e7d56490ad2c90302eab12961a22d
Mark Dzotsenidze <markdzo@hotmail.com>
2025-03-09 15:49:48 +0200

    #118 remove hupper from launcher

--- MVP/refactored/launcher.py
+++ MVP/refactored/launcher.py
@@ -2,8 +2,6 @@ import tkinter as tk
 from tkinter import font  # Import the font module
 from tkinter import messagebox
 
-import hupper
-
 from MVP.refactored.backend.diagram_callback import Receiver
 from MVP.refactored.frontend.windows.main_diagram import MainDiagram
 
@@ -69,5 +67,4 @@ def start_program():
 
 
 if __name__ == '__main__':
-    hupper.start_reloader('launcher.start_program')
     start_program()
