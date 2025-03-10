96920f89e866fcc203d3e78a383151af69650274
Mark Dzotsenidze <markdzo@hotmail.com>
2025-03-10 18:01:59 +0200

    #122 function and class docstrings

--- MVP/refactored/frontend/canvas_objects/spider.py
+++ MVP/refactored/frontend/canvas_objects/spider.py
@@ -4,7 +4,25 @@ from MVP.refactored.frontend.canvas_objects.connection import Connection
 
 
 class Spider(Connection):
+    """
+    Spider. A subclass of Connection.
+
+    Used separately from Boxes and diagram input/output. They are a freely moving canvas widget, displayed as a circle.
+    Its size can vary. It allows multiple wires to be connected to it.
+    """
     def __init__(self, location, canvas, receiver, id_=None):
+        """
+        Spider constructor.
+
+        :param location: Tuple of coordinates for creation. Ex: (10, 20)
+        :type location: tuple
+        :param canvas: CustomCanvas object that Spider will be created on.
+        :type canvas: CustomCanvas
+        :param receiver: Receiver object for backend callouts.
+        :type receiver: Receiver
+        :param id_: Optional id parameter
+        :type id_: int
+        """
         self.r = 10
         super().__init__(None, 0, "spider", location, canvas, self.r)
         self.canvas = canvas
@@ -31,9 +49,19 @@ class Spider(Connection):
         self.is_snapped = False
 
     def is_spider(self):
+        """
+        Method to check if the Connection is a Spider.
+
+        :return: True if the Connection is a Spider, False otherwise
+        """
         return True
 
     def bind_events(self):
+        """
+        Bind events to circle created on CustomCanvas.
+
+        :return: None
+        """
         self.canvas.tag_bind(self.circle, '<ButtonPress-1>', lambda event: self.on_press())
         self.canvas.tag_bind(self.circle, '<B1-Motion>', self.on_drag)
         self.canvas.tag_bind(self.circle, '<ButtonPress-3>', self.show_context_menu)
@@ -42,6 +70,13 @@ class Spider(Connection):
         self.canvas.tag_bind(self.circle, "<Leave>", lambda _: self.canvas.on_leave_hover())
 
     def show_context_menu(self, event):
+        """
+        Create and display a context menu for the selected Spider.
+
+        :param event: Event sent from keybind. Location used for context menu to be created at.
+        :type event: tkinter.Event
+        :return: None
+        """
         self.close_menu()
         self.context_menu = tk.Menu(self.canvas, tearoff=0)
 
@@ -51,6 +86,13 @@ class Spider(Connection):
         self.context_menu.tk_popup(event.x_root, event.y_root)
 
     def delete(self, action=None):
+        """
+        Delete Spider.
+
+        :param action: Specify if action is done for creating a sub-diagram.
+        :type action: str
+        :return: None
+        """
         [wire.delete_self(self) for wire in self.wires.copy()]
         self.canvas.spiders.remove(self)
         super().delete()
@@ -59,14 +101,39 @@ class Spider(Connection):
                 self.receiver.receiver_callback('delete_spider', wire_id=self.id, connection_id=self.id)
 
     def close_menu(self):
+        """
+        Close context menu if exists.
+
+        :return: None
+        """
         if self.context_menu:
             self.context_menu.destroy()
 
     def add_wire(self, wire):
+        """
+        Add wire to Spider.
+
+        This method adds a wire to the wires list if it's not already added.
+
+        :param wire: Wire that will be added to wires.
+        :type wire: Wire
+        :return: None
+        """
         if wire not in self.wires:
             self.wires.append(wire)
 
     def on_resize_scroll(self, event):
+        """
+        Handle resizing scroll keybind.
+
+        Checks the event to see if Spider needs to be made smaller or larger.
+        Checks for collisions with potential new size, if there are collisions then resizing is cancelled.
+        Changes the size of the Spider according to event.
+
+        :param event: Event object sent from key bind.
+        :type event: tkinter.Event
+        :return: None
+        """
         if event.delta == 120:
             multiplier = 1
         else:
@@ -84,6 +151,13 @@ class Spider(Connection):
 
     # MOVING, CLICKING ETC.
     def on_press(self):
+        """
+        Handle button-1 press. (Left mouse click)
+
+        Clears previously selected items and adds current Spider to new selection.
+
+        :return: None
+        """
         self.canvas.selector.selected_boxes.clear()
         self.canvas.selector.selected_spiders.clear()
         self.canvas.selector.selected_wires.clear()
@@ -96,6 +170,14 @@ class Spider(Connection):
                 self.canvas.selector.selected_items.append(self)
 
     def on_control_press(self):
+        """
+        Handle ctrl + button-1 press.
+
+        Similar to .on_press(), but does not clear earlier selections.
+        Selects/deselects based on whether Spider was selected or not.
+
+        :return: None
+        """
         if self in self.canvas.selector.selected_items:
             self.canvas.selector.selected_items.remove(self)
             self.deselect()
@@ -105,6 +187,18 @@ class Spider(Connection):
         self.canvas.selector.select_wires_between_selected_items()
 
     def on_drag(self, event):
+        """
+        Handle Spider dragging.
+
+        Uses is_illegal_move() to check for move legality. Checks if it is moving into the same x-axis as another
+        existing widget and will snap the object to the closest free space in the same x-axis as the found widget.
+        If Spider is already snapped it will also check for collision with other objects like other Spiders or Boxes
+        and would not allow movement through them.
+
+        :param event: Event object sent from key bind.
+        :type event: tkinter.Event
+        :return: None
+        """
         if event.state & 0x4:
             return
         if self.canvas.pulling_wire:
@@ -169,6 +263,16 @@ class Spider(Connection):
         [w.update() for w in self.wires]
 
     def align_wire_ends(self):
+        """
+        Method used to check if the Spider has moved to another side of a different Spider and chance variables
+        accordingly.
+
+        If the Spider has moved to the other side of a different Spider, then wire start and end connections
+        will flip. Start connection becomes end connection and vice versa. This is used to keep the wire start and end
+        connections on the left and right accordingly.
+
+        :return: None
+        """
         for connection in list(filter(lambda x: (x is not None and x != self and x.is_spider()),
                                       [w.end_connection for w in self.wires] + [w.start_connection for w in
                                                                                 self.wires])):
@@ -186,6 +290,18 @@ class Spider(Connection):
                 wire.end_connection = end
 
     def find_collisions(self, go_to_x, go_to_y):
+        """
+        Find collisions at the desired location.
+
+        Takes x and y coordinates and checks the surrounding area, equal to the size of the Spider. Returns a list
+        of canvas tags that are in the location. Wires are excluded from this.
+
+        :param go_to_x: x coordinate for the center of the search.
+        :type go_to_x: int
+        :param go_to_y: y coordinate for the center of the search.
+        :type go_to_y: int
+        :return: List of ints.
+        """
         collision = self.canvas.find_overlapping(go_to_x - self.r, go_to_y - self.r, go_to_x + self.r,
                                                  go_to_y + self.r)
         collision = list(collision)
@@ -198,6 +314,17 @@ class Spider(Connection):
         return collision
 
     def is_illegal_move(self, new_x):
+        """
+        Check if move to new_x is illegal.
+
+        Checks if the Spider can move to new_x. It is illegal when the Spider is trying to move to the other side of a
+        connected Connection. It is legal when a Spider is trying to cross over to the other side of a connected Spider,
+        but illegal when trying to go into the same x-axis as the connected Spider.
+
+        :param new_x: x coordinate that Spider is trying to move to.
+        :type new_x: int
+        :return: boolean
+        """
         for connection in list(filter(lambda x: (x is not None and x != self),
                                       [w.end_connection for w in self.wires] + [w.start_connection for w in
                                                                                 self.wires])):
@@ -212,5 +339,14 @@ class Spider(Connection):
         return False
 
     def remove_wire(self, wire=None):
+        """
+        Remove wire from Spider.
+
+        If possible remove the given wire from the Spider.
+
+        :param wire: Wire to be removed.
+        :type wire: Wire
+        :return: None
+        """
         if wire and wire in self.wires:
             self.wires.remove(wire)
