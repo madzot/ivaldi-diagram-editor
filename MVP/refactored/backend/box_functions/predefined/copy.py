d55e8f6617bbf4a89509e15b160060f84ead5c65
Mark Dzotsenidze <markdzo@hotmail.com>
2025-04-28 14:03:47 +0300

    #149 remove predefined functions

--- MVP/refactored/backend/box_functions/predefined/copy.py
+++
@@ -1,2 +0,0 @@
-def invoke(x) -> list:
-    return [x, x]
--- MVP/refactored/backend/box_functions/predefined/subtract.py
+++
@@ -1,4 +0,0 @@
-def invoke(*numbers: list) -> int:
-    if len(numbers) < 2:
-        raise ValueError("Numbers amount should be at least 2")
-    return numbers[0] - sum(numbers[1:])
